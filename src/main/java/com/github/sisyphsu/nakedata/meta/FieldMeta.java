f543033372adcc23e9b7099dc4dbf401590bf35b
sulin <sulinixl@163.com>
2019-04-24 21:41:22 +0800

    backup

---
+++ src/main/java/com/github/sisyphsu/nakedata/meta/FieldMeta.java
@@ -0,0 +1,15 @@
+package com.github.sisyphsu.nakedata.meta;
+
+/**
+ * @author sulin
+ * @since 2019-04-24 21:40:04
+ */
+public class FieldMeta {
+
+    private int nameId;
+
+    private int type;
+
+    private int refType;
+
+}
---
+++ src/main/java/com/github/sisyphsu/nakedata/meta/ObjectMeta.java
@@ -0,0 +1,13 @@
+package com.github.sisyphsu.nakedata.meta;
+
+import java.util.List;
+
+/**
+ * @author sulin
+ * @since 2019-04-24 21:39:38
+ */
+public class ObjectMeta {
+
+    private List<FieldMeta> fields;
+
+}
