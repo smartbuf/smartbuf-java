2fe5e172ef3b2a656b9317e02b5618fb93af5c8b
sulin <sulinixl@163.com>
2019-05-12 18:48:51 +0800

    暂时采用字符串的方式存储带时区的时间

---
+++ src/main/java/com/github/sisyphsu/nakedata/common/Zone.java
@@ -0,0 +1,14 @@
+package com.github.sisyphsu.nakedata.common;
+
+/**
+ * @author sulin
+ * @since 2019-05-12 18:20:32
+ */
+public enum Zone {
+
+    ACT,
+    AET,
+    AGT,
+    ART,
+
+}
---
+++ src/test/java/com/github/sisyphsu/nakedata/convertor/adaptor/AdaptorTest.java
@@ -0,0 +1,19 @@
+package com.github.sisyphsu.nakedata.convertor.adaptor;
+
+import com.github.sisyphsu.nakedata.convertor.adaptor.time.DurationAdaptor;
+import org.junit.Test;
+
+/**
+ * @author sulin
+ * @since 2019-05-12 16:26:20
+ */
+public class AdaptorTest {
+
+    @Test
+    public void testType() {
+        Adaptor adaptor = new DurationAdaptor();
+        System.out.println(adaptor.getSrcType());
+        System.out.println(adaptor.getTgtType());
+    }
+
+}
