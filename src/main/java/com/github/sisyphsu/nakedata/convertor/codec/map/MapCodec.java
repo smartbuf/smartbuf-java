95bdf316af3a25b6a1c0b847913358d8d0dd5565
sulin <sulinixl@163.com>
2019-07-13 15:01:48 +0800

    merge Map codec

--- src/main/java/com/github/sisyphsu/nakedata/convertor/codec/map/MapCodec.java
+++ src/main/java/com/github/sisyphsu/nakedata/convertor/codec/map/MapCodec.java
@@ -2,15 +2,58 @@ package com.github.sisyphsu.nakedata.convertor.codec.map;
 
 import com.github.sisyphsu.nakedata.convertor.codec.Codec;
 
-import java.util.Map;
+import java.lang.reflect.Type;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.jar.Attributes;
 
 /**
+ * Map's codec
+ *
  * @author sulin
  * @since 2019-05-13 18:30:41
  */
 public class MapCodec extends Codec {
 
-    public Map toMap() {
-        return null;
+    /**
+     * Convert Map to Map with the specified generic type
+     *
+     * @param map  Source Map
+     * @param type Generic Type
+     * @return Map
+     */
+    public <T extends Map> T toMap(Map map, Type type) {
+        if (map == null)
+            return null;
+        Class clz = (Class) type;
+        Map result;
+        if (EnumMap.class.isAssignableFrom(clz)) {
+            result = new EnumMap(clz); // TODO 取
+        } else if (HashMap.class.isAssignableFrom(clz)) {
+            result = new HashMap();
+        } else if (Hashtable.class.isAssignableFrom(clz)) {
+            result = new Hashtable();
+        } else if (IdentityHashMap.class.isAssignableFrom(clz)) {
+            result = new IdentityHashMap();
+        } else if (LinkedHashMap.class.isAssignableFrom(clz)) {
+            result = new LinkedHashMap();
+        } else if (Properties.class.isAssignableFrom(clz)) {
+            result = new Properties();
+        } else if (TreeMap.class.isAssignableFrom(clz)) {
+            result = new TreeMap();
+        } else if (WeakHashMap.class.isAssignableFrom(clz)) {
+            result = new WeakHashMap(map.size());
+        } else if (ConcurrentHashMap.class.isAssignableFrom(clz)) {
+            result = new ConcurrentHashMap(map.size());
+        } else if (ConcurrentSkipListMap.class.isAssignableFrom(clz)) {
+            result = new ConcurrentSkipListMap();
+        } else if (Attributes.class.isAssignableFrom(clz)) {
+            result = new Attributes(map.size());
+        } else {
+            throw new RuntimeException("");
+        }
+        return (T) result;
     }
+
 }
--- src/main/java/com/github/sisyphsu/nakedata/convertor/codec/map/MapEntryCodec.java
+++ src/main/java/com/github/sisyphsu/nakedata/convertor/codec/map/MapEntryCodec.java
@@ -2,15 +2,48 @@ package com.github.sisyphsu.nakedata.convertor.codec.map;
 
 import com.github.sisyphsu.nakedata.convertor.codec.Codec;
 
+import java.lang.reflect.Type;
+import java.util.AbstractMap;
 import java.util.Map;
 
 /**
+ * Map.Entry's codec
+ *
  * @author sulin
  * @since 2019-05-13 18:37:34
  */
 public class MapEntryCodec extends Codec {
 
-    public Map.Entry toMapEntry() {
+    /**
+     * Convert Map.Entry to Map.Entry with the specified generic type
+     *
+     * @param entry Map.Entry
+     * @param type  Type
+     * @return Map.Entry
+     */
+    public <T extends Map.Entry> T toMapEntry(Map.Entry entry, Type type) {
+        if (entry == null)
+            return null;
+        Class clz = (Class) type;
+        Map.Entry result;
+        if (AbstractMap.SimpleEntry.class.isAssignableFrom(clz)) {
+            result = new AbstractMap.SimpleEntry(null, null);
+        } else if (AbstractMap.SimpleImmutableEntry.class.isAssignableFrom(clz)) {
+            result = new AbstractMap.SimpleImmutableEntry(null, null);
+        } else {
+            throw new RuntimeException("");
+        }
+        return (T) result;
+    }
+
+    public Map.Entry toMapEntry(Map map, Type type) {
+        // TODO 将map转换为entry，然后再转换为entry<?,?>
         return null;
     }
+
+    public Map toMap(Map.Entry entry, Type type) {
+        // TODO wrap entry as map
+        return null;
+    }
+
 }
