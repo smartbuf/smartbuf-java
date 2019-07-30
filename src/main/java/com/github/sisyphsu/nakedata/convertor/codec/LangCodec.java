a99bfc680e0376b490299c5c2f91834da09daebd
sulin <sulinixl@163.com>
2019-07-30 11:14:44 +0800

    implement codec for object <-> map

--- src/main/java/com/github/sisyphsu/nakedata/convertor/codec/LangCodec.java
+++ src/main/java/com/github/sisyphsu/nakedata/convertor/codec/LangCodec.java
@@ -2,7 +2,9 @@ package com.github.sisyphsu.nakedata.convertor.codec;
 
 import com.github.sisyphsu.nakedata.convertor.Codec;
 import com.github.sisyphsu.nakedata.convertor.Converter;
+import com.github.sisyphsu.nakedata.convertor.reflect.XField;
 import com.github.sisyphsu.nakedata.convertor.reflect.XType;
+import net.sf.cglib.beans.BeanMap;
 
 import java.text.SimpleDateFormat;
 import java.util.Map;
@@ -16,24 +18,34 @@ import java.util.Map;
 public class LangCodec extends Codec {
 
     /**
-     * TODO implementation
-     *
-     * @param map
-     * @param type
-     * @return
+     * Convert Map to Object
      */
-    public Object toObject(Map map, XType type) {
+    public Object toObject(Map<?, ?> map, XType<?> type) {
         if (type.getRawType() == Object.class) {
-            return map;
+            return map; // dont need convert
         }
+        Object result;
         try {
-            Object result = type.getRawType().newInstance();
-
-            return result;
+            result = type.getRawType().newInstance();
         } catch (Exception e) {
-
+            throw new IllegalArgumentException("Initialize " + type.getRawType() + " failed: ", e);
         }
-        return null;
+        BeanMap bm = BeanMap.create(result);
+        for (Map.Entry<String, XField> entry : type.getFields().entrySet()) {
+            Object data = map.get(entry.getKey());
+            if (data == null) {
+                continue;
+            }
+            bm.put(entry.getKey(), convert(data, entry.getValue().getType()));
+        }
+        return result;
+    }
+
+    /**
+     * Convert Object to Map, use cglib directly
+     */
+    public Map toMap(Object obj) {
+        return BeanMap.create(obj);
     }
 
     /**
